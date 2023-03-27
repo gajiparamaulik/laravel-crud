c1fdf1d2549071927dd833cd28e11f595d90fdc6
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-03-27 22:14:45 +0530

    start header fix

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,7 +8,8 @@
 	<title>Bootstrap CRUD Data Table for Database with Modal Form</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -18,18 +19,46 @@
 		<div class="container">
 			<div class="table-wrapper">
 				<div class="table-title">
-					<div class="row">
-						<div class="col-sm-6">
-							<h2>Manage <b>Products</b></h2> </div>
-						<div class="col-sm-6"> <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">
-                            <i class="material-icons">&#xE147;</i> 
-                            <span>Add New Product</span></a> 
-                            <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal">
-                                <i class="material-icons">&#xE15C;</i> 
-                                <span>Delete</span>
-                            </a> 
-                        </div>
+					<div class="d-flex bd-highlight">
+						<div class="p-2 flex-grow-1 bd-highlight">
+							<h2>Manage <b>Products</b></h2> 
+						</div>
+						<div class="p-2 bd-highlight">
+							<div class="col-xs-2">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+						</div>
+						<div class="p-2 bd-highlight"> 
+							<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">
+								<i class="material-icons">&#xE147;</i> 
+								<span>Add Product</span>
+							</a>
+						</div>
 					</div>
+
+					{{-- <div class="row">
+						<div class="col-sm-6">
+							<h2>Manage <b>Products</b></h2> 
+						</div>
+						<div class="col-sm-6"> 
+							<div class="d-flex justify-content-center">
+								<div class="p-2 bd-highlight">
+									<div class="col-xs-5">
+										<input type="text" class="form-control" placeholder="Search">
+									</div>
+								</div>
+								<div class="p-2 bd-highlight">
+									<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">
+										<i class="material-icons">&#xE147;</i> 
+										<span>Add Product</span>
+									</a>
+								</div>
+								<div class="p-2 bd-highlight">
+
+								</div>
+							</div>
+                        </div>
+					</div> --}}
 				</div>
 				<table class="table table-striped table-hover">
 					<thead>
