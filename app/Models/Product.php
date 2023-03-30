7e5e75400f9a9108286a9c1e0c59ae988eeab39c
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-03-30 09:32:22 +0530

    product table design done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,4 +16,9 @@ class Product extends Model
         'thumbnail',
         'details'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -20,35 +20,44 @@
 		</div>
 	</nav>
 
-	<table class="table  table-dark">
-		<thead>
-			<tr>
-				<th scope="col">#</th>
-				<th scope="col">First</th>
-				<th scope="col">Last</th>
-				<th scope="col">Handle</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th scope="row">1</th>
-				<td>Mark</td>
-				<td>Otto</td>
-				<td>@mdo</td>
-			</tr>
-			<tr>
-				<th scope="row">2</th>
-				<td>Jacob</td>
-				<td>Thornton</td>
-				<td>@fat</td>
-			</tr>
-			<tr>
-				<th scope="row">3</th>
-				<td colspan="2">Larry the Bird</td>
-				<td>@twitter</td>
-			</tr>
-		</tbody>
-	</table>
+	<div class="container bg-light mt-3">
+		<table class="table ">
+			<thead>
+				<tr>
+					<th scope="col">No.</th>
+					<th scope="col">User Id</th>
+					<th scope="col">Name</th>
+					<th scope="col">Type</th>
+					<th scope="col">Thumbnail</th>
+					<th scope="col">Details</th>
+					<th scope="col">Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($getData as $data)
+				<tr>
+					<th scope="row">1</th>
+					<td>{{ $data->user->name }}</td>
+					<td>{{ $data['name'] }}</td>
+					<td>{{ $data['type'] }}</td>
+					<td><img src="images/{{ $data['thumbnail'] }}" height="30" alt=""></td>
+					<td>{{ $data['details'] }}</td>
+					<td>
+						<a class="btn btn-info" href="">Show</a>
+    
+                    <a class="btn btn-primary" href="">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+	
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
 	  	integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
