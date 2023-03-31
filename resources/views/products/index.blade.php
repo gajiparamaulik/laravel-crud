19fd2b46d7decf1e70e51fca018dcec212c1549f
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-03-31 09:31:16 +0530

    add static modal

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,8 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Products</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-		integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="{{asset('css/product.css')}}">
 	<script src="{{asset('js/product.js')}}"></script>
 </head>
@@ -19,6 +20,10 @@
 			</div>
 		</div>
 	</nav>
+
+	<div class="container mt-3">
+		<button class="btn btn-dark"  data-bs-toggle="modal" data-bs-target="#formModal">Add Product</button>
+	</div>
 
 	<div class="container bg-light mt-3">
 		<table class="table ">
@@ -43,24 +48,40 @@
 					<td><img src="images/{{ $data['thumbnail'] }}" height="30" alt=""></td>
 					<td>{{ $data['details'] }}</td>
 					<td>
-						<a class="btn btn-info" href="">Show</a>
-    
-                    <a class="btn btn-primary" href="">Edit</a>
-   
-                    @csrf
-                    @method('DELETE')
-      
-                    <button type="submit" class="btn btn-danger">Delete</button>
+						<a href="#editEmployeeModal" class="edit" data-toggle="modal">
+							<i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
+						</a>
+						<a href="#deleteEmployeeModal" class="delete" data-toggle="modal">
+							<i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+						</a>
 					</td>
 				</tr>
 				@endforeach
 			</tbody>
 		</table>
 	</div>
+
+	<!-- Modal -->
+	<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+					<button	button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+				
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
 	
 
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
-	  	integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-		
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+	integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>		
 </body>
 </html>
