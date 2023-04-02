c18d1d3cb6e9600ce910c28eac3a5ce34b514a15
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-04-02 20:54:19 +0530

    add new products function complete

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Products</title>
+	<link rel="icon" type="image/x-icon" href="/images/mg-logo.jpg">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
 	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -39,9 +40,9 @@
 				</tr>
 			</thead>
 			<tbody>
-				@foreach($getData as $data)
+				@foreach($getData as $index => $data)
 				<tr>
-					<th scope="row">1</th>
+					<th scope="row">{{ $index + 1 }}</th>
 					<td>{{ $data->user->name }}</td>
 					<td>{{ $data['name'] }}</td>
 					<td>{{ $data['type'] }}</td>
@@ -66,38 +67,35 @@
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+					<h5 class="modal-title" id="exampleModalLabel">Add Products</h5>
 					<button	button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div class="modal-body">
-					<form>
+					<form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+						@csrf
 						<div class="mb-3">
-						  <label for="name" class="form-label">Full Name</label>
-						  <input type="text" class="form-control" name="name">
+						  <label for="name" class="form-label fw-bolder">Full Name</label>
+						  <input type="text" class="form-control" name="name" placeholder="Enter Full Name">
 						</div>
 						<div class="mb-3">
-							<label for="name" class="form-label">Type</label>
-							<select class="form-select">
+							<label for="name" class="form-label fw-bolder">Type</label>
+							<select class="form-select" name="type">
 							  <option selected>Choose...</option>
-							  <option value="1">One</option>
-							  <option value="2">Two</option>
-							  <option value="3">Three</option>
+							  <option value="one">One</option>
+							  <option value="two">Two</option>
+							  <option value="three">Three</option>
 							</select>
 						</div>
 						<div class="mb-3">
-						  <label for="name" class="form-label">Thumbnail</label>
-						  <input type="file" accept=".png, .jpg, .jpeg, .gif" />
+						  <label for="name" class="form-label fw-bolder">Thumbnail</label><br>
+						  <input type="file" accept=".png, .jpg, .jpeg, .gif" name="thumbnail" />
 						</div>
-						<div class="form-floating mb-3">
-							<label for="floatingTextarea2" class="form-label">Details</label>
-							<textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-						  </div>
-						<button type="submit" class="btn btn-primary">Submit</button>
+						<div class="form-group purple-border">
+							<label for="details" class="fw-bolder">Details</label>
+							<textarea class="form-control" rows="3" name="details" placeholder="Leave a comment here"></textarea>
+						</div>
+						<button type="submit" class="btn btn-primary float-right mt-2">Submit</button>
 					  </form>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
 				</div>
 			</div>
 		</div>
