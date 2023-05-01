4c400f7bfdaf497484ddfb9718014c30a26c9e82
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-05-01 21:07:00 +0530

    type update

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -34,7 +34,7 @@
 					<th scope="col">No. check2</th>
 					<th scope="col">User Id check list add2</th>
 					<th scope="col">Name check22</th>
-					<th scope="col">Type check</th>
+					<th scope="col">Type check22</th>
 					<th scope="col">Thumbnail check</th>
 					<th scope="col">Details check</th>
 					<th scope="col">Actions check</th>
