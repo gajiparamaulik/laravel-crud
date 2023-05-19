2f1961250c05038506d68d5c73e3f8a672a1a9f8
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-05-19 09:26:56 +0530

    add modal

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -83,7 +83,6 @@
 								<option value="three">Three</option>
 							</select>
 						</div> 
-
 						<div class="mb-3">
 							<label for="name" class="form-label fw-bolder">Thumbnail</label><br>
 							<input type="file" accept=".png, .jpg, .jpeg, .gif" name="thumbnail"  />
