cd6db8a99113beeedeafb918aa5a44d499cc64ae
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-06-08 20:30:39 +0530

    resources route update

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('/products', ProductController::class)->middleware(['auth']);
 
 
+
 Route::get('/send-mail', [MailController::class, 'index']);
 
 Route::get('user_details', [App\Http\Controllers\LocationController::class, 'userDetails']);
